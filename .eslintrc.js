9d433aa677446f21583cb74b1e378fc0fe80d95f
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-10-11 10:57:51 +0300

    Added file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+      node: true,
+    },
+    files: ['.eslintrc.{js,cjs}'],
+    parserOptions: {
+      sourceType: 'script',
+    },
+    extends: ['eslint:recommended'],
+    rules: {},
+  };
